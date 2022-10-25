05bc531b4951261dcf780eca064b454f2cc91595
chiragbansal10 <98810248+chiragbansal10@users.noreply.github.com>
2022-10-25 10:59:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,49 +1,41 @@
-
-import "./categories.scss"
-
+// import "./categories.scss";
+// import Category from "./Components/Category/Category";
+import Categories from "./Components/Category/Categories";
+import {Routes,Route} from "react-router-dom";
+import Navigation from "./Components/Navigation/Navigation";
+import NoMatchComponent from "./Components/NoMatchComponent/NoMatchComponent";
+import Hats from "./Products/Hats";
+import Jackets from "./Products/Jackets";
 function App() {
-  const categorie=[
-    {
-      title: "Hats",
-      imageUrl:'https://i.ibb.co/cvpntL1/hats.png',
-    },
+  
 
-    {
-      title: "Jackets",
-      imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      title: "Sneakers",
-      imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      title: "Womens",
-      imageUrl:'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      title: "Mens",
-      imageUrl:'https://i.ibb.co/R70vBrQ/mens.png',
-    }
-  ]
   return (
     <div className="App">
-      <div className='categories'>
-     {categorie.map(({title,imageUrl})=>(
-        <div className='category-container' style={{backgroundImage :`url(${imageUrl})`}}>
-         
-<div className='category-body'>
-  <h2>{title}</h2>
-  <p>Shop Now</p>
-</div>
-        </div>
+     
+      <Routes>
+      
+      <Route path="/" element={ <Navigation />} >
+      <Route path="/categories" element={ <Categories />} />
+  
+      
+      <Route path="/categories/hats" element={ <Hats />} />
+      <Route path="/categories/jackets" element={ <Jackets />} />
+      </Route>
+      
+      <Route path="*" element={ <NoMatchComponent />} />
+      <Route path="/test" element={ <Sample />} />
+      </Routes>
 
-      ))} 
-        
-        
-        </div>
       </div>
     
   );
+}
+const Sample= ()=>{
+  return(
+    <div>
+      <h1>Sample</h1>
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./style1.scss";
 import App from './App';
+import "./index.scss";
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App></App>,document.getElementById("root"))
+
+ReactDOM.render(
+    <BrowserRouter>
+<App/> </BrowserRouter>,document.getElementById('root'));
+
+
+
+
